2aed8e70affcbc391884d9f0c936265c8edd54ba
blrik <blrik@live.ru>
2017-03-04 05:31:17 +0300

    добавил ejs

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+app.use(express.static('./app/public'));
+
 
 //error
 app.use(function(req, res, next) {
